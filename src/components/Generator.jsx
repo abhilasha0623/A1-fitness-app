e6a088e90e9271eeaa30738d318130cfc6e61524
abhilasha0623 <abhilasha.92.kp@gmail.com>
2025-02-12 19:09:00 +0530

    button for workouts

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,24 +1,46 @@
-import React from 'react'
-import SectionWrapper from './SectionWrapper'
+import React from "react";
+import SectionWrapper from "./SectionWrapper";
+import { WORKOUTS } from "../utils/swoldier";
 
-
-function Header(props){
-  const {index, title, description} = props;
+function Header(props) {
+  const { index, title, description } = props;
   return (
-    <div className='flex flex-col gap-4'>
-        <div className='flex items-center justify-center gap-2'>
-            <p className='text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400'>{index}</p>
-            <h4 className='text-xl sm:text-2xl md:text-3xl'>{title}</h4>
-        </div>
-        <p className='text-sm sm:text-base mx-auto'>{description}</p>
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-center gap-2">
+        <p className="text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400">
+          {index}
+        </p>
+        <h4 className="text-xl sm:text-2xl md:text-3xl">{title}</h4>
+      </div>
+      <p className="text-sm sm:text-base mx-auto">{description}</p>
     </div>
-)
+  );
 }
 export default function Generator() {
   return (
-    <SectionWrapper header={"generate yor workout"} 
-    title={['It\'s' ,'Huge' , 'o\'clock']}>
-      <Header index={'01'} title={'Pick your Poison'} description={'Select the workout you wish to endure.'}/>
+    <SectionWrapper
+      header={"generate yor workout"}
+      title={["It's", "Huge", "o'clock"]}
+    >
+      <Header
+        index={"01"}
+        title={"Pick your Poison"}
+        description={"Select the workout you wish to endure."}
+      />
+      <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+        {Object.keys(WORKOUTS).map((workout, index) => {
+          return (
+            <button
+              className={
+                "bg-slate-950 border  duration-200 px-6 hover:border-blue-600 py-6 rounded-lg "
+              }
+              key={index}
+            >
+              <p>{workout}</p>
+            </button>
+          );
+        })}
+      </div>
     </SectionWrapper>
-  )
+  );
 }
